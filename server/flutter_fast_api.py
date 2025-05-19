e7702a27aa152e2a6d6b9365bfaeb764aaf7f22f
mindoll1101 <mindoll1101@kangwon.ac.kr>
2025-05-19 21:08:49 +0900

    사용자 정보 인증, 새로운 장소 추가 기능 구현

--- server/flutter_fast_api.py
+++ server/flutter_fast_api.py
@@ -23,39 +23,35 @@ app = FastAPI()
 #     database='route_recommendation',
 #     charset='utf8mb4',
 # )
-# origins = [
-#     "http://localhost:8000",
-#     "http://127.0.0.1:8000",
-# ]
+hostname = socket.gethostname()
+privateIP = 'http://' + socket.gethostbyname(hostname) + ':8000/'
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,  # 허용할 출처
-#     allow_credentials=True,  # 인증 정보 포함 여부
-#     allow_methods=["*"],  # 허용할 HTTP 메서드 (GET, POST 등)
-#     allow_headers=["*"],  # 허용할 헤더
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=privateIP,  # 허용할 출처
+    allow_credentials=True,  # 인증 정보 포함 여부
+    allow_methods=["*"],  # 허용할 HTTP 메서드 (GET, POST 등)
+    allow_headers=["*"],  # 허용할 헤더
+)
 
-# is_connected = False
-# hostname = socket.gethostname()
-# privateIP = 'http://' + socket.gethostbyname(hostname) + ':8000/'
-# udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   #udp 소켓 생성. IPv4
-# udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)    #udp 소켓이 브로드캐스트 메시지를 보내도록 설정
-# def broadcast():
-#     while not is_connected:
-#         print("send broadcast message")
-#         udp_socket.sendto(privateIP.encode(), ("255.255.255.255", 8888))
-#         time.sleep(5)
-# broadcast_thread = threading.Thread(target=broadcast, daemon=True)
-# broadcast_thread.start()
-# conn = sql.connect(
-#     host='127.0.0.1',
-#     user='root',
-#     password=os.getenv("DB_PASSWORD"),
-#     database='route_recommendation',
-#     charset='utf8mb4',
-# )
-# cursor = conn.cursor()
+is_connected = False
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   #udp 소켓 생성. IPv4
+udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)    #udp 소켓이 브로드캐스트 메시지를 보내도록 설정
+def broadcast():
+    while not is_connected: 
+        udp_socket.sendto(privateIP.encode(), ("255.255.255.255", 8888))
+        time.sleep(5)
+broadcast_thread = threading.Thread(target=broadcast, daemon=True)
+broadcast_thread.start()
+conn = sql.connect(
+    host='127.0.0.1',
+    user='root',
+    password=os.getenv("DB_PASSWORD"),
+    database='route_recommendation',
+    charset='utf8mb4',
+)
+cursor = conn.cursor()
 
 
 @app.get("/")
@@ -70,44 +66,58 @@ async def read_list(category: str, x: float, y:float):
     results = await process_category(category, x, y)
     return JSONResponse(content=results, media_type="application/json; charset=utf-8")
 
+@app.post("/insert_new_place")
+async def insert_new_place(placeName: str, userID: str, startDate: str, endDate: str):
+    try:
+        cursor.execute("insert into place_list (place_name, id, start_date, end_date) values (%s, %s, %s, %s)", (placeName, userID, startDate, endDate))
+        conn.commit()
+        cursor.execute("select * from place_list")
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        print(f'error {e}')
+
 @app.post("/insert_user_info")
 async def insert_user_info(userID: str, userPW: str):
-    # try:
-    #     print(f'{userID}, {userPW}')
-    #     cursor.execute(f"insert into users_info (id, password) values ('{userID}', '{userPW}')")
-    #     conn.commit()
-    #     cursor.execute("select * from users_info")
-    #     result = cursor.fetchall()
-    #     conn.close()
-    # except Exception as e:
-    #     print(f'error {e}')
-    # finally:
-    #     print(result)
-    # return result
+    try:
+        print(f'{userID}, {userPW}')
+        cursor.execute("insert into users_info (id, password) values (%s, %s)", (userID, userPW))
+        conn.commit()
+        cursor.execute("select * from users_info")
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        print(f'error {e}')
 
-    pass
+@app.get("/user_validation")
+async def user_validation(userID: str, userPW: str):
+    try:
+        cursor.execute("select id, password from users_info where id = %s and password = %s", (userID, userPW))
+        result = cursor.fetchall()
+        if not result:
+            return False
+        else:
+            return True
+    except Exception as e:
+        print(f'uesr validation error {e}')
 
 @app.get("/get_connect_state")
 async def get_connect_state():
-    global is_connected
-    is_connected = True
-    # load_dotenv()  # .env 파일 로드
     return JSONResponse(content={"message": "확인"}, media_type="application/json; charset=utf-8")
 
 @app.get("/duplicate_check")
 async def checkIdDuplicate(userID: str):
-    # try:
-    #     isDuplicate = False
-    #     print(userID)
-    #     cursor.execute(f"select id from users_info where id = '{userID}'")
-    #     result = cursor.fetchall()
-    #     if result:
-    #         isDuplicate = True
-    #     print(result)
-    # except Exception as e:
-    #     print(f'error {e}')
-    # return isDuplicate
-    pass
+    try:
+        isDuplicate = False
+        print(userID)
+        cursor.execute("select id from users_info where id = %s", (userID,))
+        result = cursor.fetchall()
+        if result:
+            isDuplicate = True
+        print(result)
+    except Exception as e:
+        print(f'error {e}')
+    return isDuplicate
 
 @app.get("/user_info/")
 async def get_user_info():
