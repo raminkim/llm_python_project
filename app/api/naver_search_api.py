50b6a95ef8e3eeff223eed2b465fae806cee1e8c
raminKim <151427064+raminkim@users.noreply.github.com>
2025-06-30 01:32:39 +0900

    Feat: 개선된 키워드 매칭·예외 처리로 리뷰 수집 성공률 향상

--- app/api/naver_search_api.py
+++ app/api/naver_search_api.py
@@ -6,6 +6,7 @@ import os
 from urllib.parse import quote
 
 def naver_search_api(keyword):
+    print(f"naver search keyword: {keyword}")
     enc_query = quote(keyword)  # 검색어를 URL 인코딩합니다.
     url = f'https://openapi.naver.com/v1/search/local.json?query={enc_query}&display=5'
     headers = {
@@ -17,5 +18,11 @@ def naver_search_api(keyword):
         response = requests.get(url, headers=headers)
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f"API 요청 에러: {e}")
+        print(f"NAVER SEARCH API 요청 에러: {e}")
         return None
+    
+
+if __name__ == '__main__':
+    # 기존 단일 검색 테스트
+    result1 = naver_search_api("맘스터치 강원대점")
+    print("단일 검색 결과:", result1)
